f0735eceae2ba314cd197598b86db7026fabcd8f
mohammed-najib <mohammed.njeeb@gmail.com>
2022-08-03 12:42:59 +0300

    implemented protected routes

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,27 +1,32 @@
-const express = require('express');
+const express = require("express");
+
 const {
-    getAllWorkouts,
-    getWorkout,
-    createWorkout,
-    deleteWorkout,
-    updateWorkout,
-} = require('../controllers/workoutController');
+  getAllWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
 
+// require auth for all workout routes
+router.use(requireAuth);
+
 // GET all workouts
-router.get('/', getAllWorkouts);
+router.get("/", getAllWorkouts);
 
 // GET a single workout
-router.get('/:id', getWorkout);
+router.get("/:id", getWorkout);
 
 // POST a new workout
-router.post('/', createWorkout);
+router.post("/", createWorkout);
 
 // DELETE a workout
-router.delete('/:id', deleteWorkout);
+router.delete("/:id", deleteWorkout);
 
 // UPDATE a workout
-router.patch('/:id', updateWorkout);
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
